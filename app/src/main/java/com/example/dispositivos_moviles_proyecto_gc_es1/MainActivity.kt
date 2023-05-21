12bc80a3e6a9cdbe053b8d058b27bda347e366a6
easolano98 <solano_erick@hotmail.com>
2023-05-21 10:13:44 -0500

    Dispositivos Moviles solano-conlago

---
+++ app/src/main/java/com/example/dispositivos_moviles_proyecto_gc_es1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dispositivos_moviles_proyecto_gc_es1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
